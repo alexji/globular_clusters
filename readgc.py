fcf103d0fc4b9c136714009879605599f4b3ed5e
Alexander Ji <alexji@mit.edu>
2018-01-09 10:15:33 -0800

    Fixed ID problem

--- readgc.py
+++ readgc.py
@@ -4,11 +4,11 @@ from astropy.io import ascii
 
 def load_master_table():
     df1 = ascii.read("mwgc1.dat").to_pandas()
-    df1.index = df1["ID"]
+    df1.index = df1.pop("ID")
     df2 = ascii.read("mwgc2.dat").to_pandas()
-    df2.index = df2["ID"]
+    df2.index = df2.pop("ID")
     df3 = ascii.read("mwgc3.dat").to_pandas()
-    df3.index = df3["ID"]
+    df3.index = df3.pop("ID")
     df = pd.concat([df1,df2,df3], axis=1)
     return df
 
